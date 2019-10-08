265a8a3abf9b4ebde48de34154b569efff638186
kiselevgleb <kiselevgleb@gmail.com>
2019-10-08 10:26:12 +0300

    1 0 fix

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -1,19 +1,22 @@
 package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
-public class UserMealWithExceed {
+//@Entity
+//@Table (name = "mealTo")
+public class MealTo {
+//    @Column(name = "dateTime")
     private final LocalDateTime dateTime;
 //    @Column(name = "description")
     private final String description;
 //    @Column(name = "calories")
     private final int calories;
 
-    private final boolean exceed;
+    private final boolean excess;
 
-    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-        this.exceed = exceed;
+        this.excess = excess;
     }
 }
