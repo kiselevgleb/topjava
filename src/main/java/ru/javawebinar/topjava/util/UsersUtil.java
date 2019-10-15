74b85512da95e333e86221d41e59a60284c82072
kiselevgleb <kiselevgleb@gmail.com>
2019-10-15 14:03:00 +0300

    HW2 V4 15-10-19

--- src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -17,44 +17,26 @@ import static ru.javawebinar.topjava.model.Role.ROLE_ADMIN;
 import static ru.javawebinar.topjava.model.Role.ROLE_USER;
 
 public class UsersUtil {
-    public static Set<Role> roleUser=null;
-    public static Set<Role> roleAdmin=null;
-    public static Set<Role> roles=null;
+    public static Set<Role> roleUser = null;
+    public static Set<Role> roleAdmin = null;
+    public static Set<Role> roles = null;
+
     {
         roleUser.add(ROLE_USER);
         roleAdmin.add(ROLE_ADMIN);
         roles.add(ROLE_USER);
         roles.add(ROLE_ADMIN);
     }
+
     public static final List<User> USERS = Arrays.asList(
-            new User(2, "user","1@1.ru", "123", 2000, true, roleUser),
-            new User(1, "admin","2@2.ru", "123", 2500, true, roleAdmin)
-            );
-public static List<User> getSortListUser (List<User> users){
-    Comparator<User> userInitialsComparator = Comparator.comparing(User::getName);
-    Collections.sort(users, userInitialsComparator);
-    return users;
-}
-//    public static List<MealTo> getTos(Collection<Meal> meals, int caloriesPerDay) {
-//        return getFiltered(meals, caloriesPerDay, meal -> true);
-//    }
+            new User(2, "user", "1@1.ru", "123", 2000, true, roleUser),
+            new User(1, "admin", "2@2.ru", "123", 2500, true, roleAdmin)
+    );
 
-//    public static List<MealTo> getFilteredTos(Collection<Meal> meals, int caloriesPerDay, LocalTime startTime, LocalTime endTime) {
-//        return getFiltered(meals, caloriesPerDay, meal -> DateTimeUtil.isBetween(meal.getTime(), startTime, endTime));
-//    }
+    public static List<User> getSortListUser(List<User> users) {
+        Comparator<User> userInitialsComparator = Comparator.comparing(User::getName);
+        Collections.sort(users, userInitialsComparator);
+        return users;
+    }
 
-//    private static List<MealTo> getFiltered(Collection<Meal> meals, int caloriesPerDay, Predicate<Meal> filter) {
-//        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
-//                .collect(
-//                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
-//                );
-//        return meals.stream()
-//                .filter(filter)
-//                .map(meal -> createTo(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
-//                .collect(Collectors.toList());
-//    }
-//
-//    private static MealTo createTo(Meal meal, boolean excess) {
-//        return new MealTo(meal.getId(), meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
-//    }
 }
