5d54dc9b444cb61646f77ab4a9ea1c9ab666b377
kiselevgleb <kiselevgleb@gmail.com>
2019-10-15 14:43:40 +0300

    2 4 add spring context

---
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -0,0 +1,66 @@
+package ru.javawebinar.topjava.repository.inmemory;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class InMemoryMealRepository implements MealRepository {
+    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+//    {
+//        MealsUtil.MEALS.forEach(this::save);
+//    }
+
+    @Override
+    public Meal save(Meal meal) {
+        if (meal.isNew()) {
+            meal.setId(counter.incrementAndGet());
+            repository.put(meal.getId(), meal);
+            return meal;
+        }
+        // treat case: update, but not present in storage
+        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        if (repository.get(id).getUserId() == userId) {
+            return repository.remove(id) != null;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        if (repository.get(id).getUserId() == userId) {
+            return repository.get(id);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Collection<Meal> getAll(int userId) {
+        List<Meal> userMeals = new ArrayList<>();
+        if (repository.values().size()!=0){
+        for (Meal userMeal : repository.values()) {
+            if (userMeal.getUserId() == userId) {
+                userMeals.add(userMeal);
+            }
+//            else if (userMeal.getUserId() == 2){
+//                userMeals.add(userMeal);
+//            }
+        }}
+        Comparator<Meal> userInitialsComparator = Comparator.comparing(Meal::getDateTime);
+        Collections.sort(userMeals, userInitialsComparator);
+        return userMeals;
+    }
+
+}
+
---
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -0,0 +1,63 @@
+package ru.javawebinar.topjava.repository.inmemory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.UsersUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        UsersUtil.USERS.forEach(this::save);
+    }
+
+    @Override
+    public User save(User user) {
+//        if (user.isNew()) {
+//            user.setId(counter.incrementAndGet());
+        repository.put(user.getId(), user);
+        return user;
+//        }
+        // treat case: update, but not present in storage
+//        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return true;
+    }
+
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        return repository.values().stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        return null;
+    }
+}
