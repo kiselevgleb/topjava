db0b3e9861fd4bb923991692167c0a02a3c2c443
kiselevgleb <kiselevgleb@gmail.com>
2019-10-24 12:56:36 +0300

    23-10 15-10 pom.xml

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,34 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+
+public class MealServiceTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenDates() {
+    }
+
+    @Test
+    public void getAll() {
+
+//        List<Meal> expected = MealRepository.repository.getAll(userId);
+//int actual = 3;
+//        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+    }
+}
