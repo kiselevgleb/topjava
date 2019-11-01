809d5b5fbceefbb7567f43b9f7f8eadbd58bb459
kiselevgleb <kiselevgleb@gmail.com>
2019-11-01 13:23:47 +0300

    verified with Master topjava

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -1,65 +1,39 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.UserTestData.USER;
+
+
 @Repository
-public class InMemoryUserRepository implements UserRepository {
+public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
 
-    public static final int USER_ID = 1;
-    public static final int ADMIN_ID = 2;
-
-    private Map<Integer, User> usersMap = new ConcurrentHashMap<>();
-    private AtomicInteger counter = new AtomicInteger(0);
-
-    @Override
-    public User save(User user) {
-        if (user.isNew()) {
-            user.setId(counter.incrementAndGet());
-            usersMap.put(user.getId(), user);
-            return user;
-        }
-        return usersMap.computeIfPresent(user.getId(), (id, oldUser) -> user);
-    }
-
-//    @Override
-//    public boolean delete(int id) {
-//        return usersMap.remove(id) != null;
-//    }
-//
-//    @Override
-//    public boolean delete(int id) {
-//        return usersMap.remove(id) != null;
-//    }
-
-    @Override
-    public boolean delete(int id) {
-        return usersMap.remove(id) != null;
-    }
-
-    @Override
-    public User get(int id) {
-        return usersMap.get(id);
+    public void init() {
+        map.clear();
+        map.put(UserTestData.USER_ID, USER);
+        map.put(UserTestData.ADMIN_ID, ADMIN);
     }
 
     @Override
     public List<User> getAll() {
-        return usersMap.values().stream()
+        return getCollection().stream()
                 .sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail))
                 .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
-        return usersMap.values().stream()
+        Objects.requireNonNull(email, "email must not be null");
+        return getCollection().stream()
                 .filter(u -> email.equals(u.getEmail()))
                 .findFirst()
                 .orElse(null);
