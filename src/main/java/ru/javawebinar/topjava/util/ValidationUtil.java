eea85f4362ac7ddbe8a43c6aa6fe2460f9ec5d7f
kiselevgleb <kiselevgleb@gmail.com>
2019-12-26 14:24:49 +0300

    9 07 jdbc validation

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -5,6 +5,10 @@ import org.springframework.validation.BindingResult;
 import ru.javawebinar.topjava.HasId;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import javax.validation.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class ValidationUtil {
 
     private ValidationUtil() {
@@ -53,5 +57,22 @@ public class ValidationUtil {
             result = cause;
         }
         return result;
+    }
+
+    private static final Validator validator;
+
+    static {
+        //  From Javadoc: implementations are thread-safe and instances are typically cached and reused.
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        //  From Javadoc: implementations of this interface must be thread-safe
+        validator = factory.getValidator();
+    }
+
+    public static <T> void validate(T bean) {
+        // https://alexkosarev.name/2018/07/30/bean-validation-api/
+        Set<ConstraintViolation<T>> violations = validator.validate(bean);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
     }
 }
