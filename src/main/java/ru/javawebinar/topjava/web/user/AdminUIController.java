c2a8a277986e75ada0f1a0f28c10b5a766a1d719
kiselevgleb <kiselevgleb@gmail.com>
2019-12-26 14:24:15 +0300

    9 06 validation

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -2,11 +2,14 @@ package ru.javawebinar.topjava.web.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.ValidationUtil;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -42,6 +45,7 @@ public class AdminUIController extends AbstractUserController {
         } else {
             super.update(userTo, userTo.id());
         }
+        return ResponseEntity.ok().build();
     }
 
     @Override
